8ddcc9966f36c48f9b8386c174c70eb224a43722
paolo marzolo <pollo.marzo@gmail.com>
2025-02-27 00:14:55 +0100

    started working on "thoughts".. hyped

---
+++ src/content.config.ts
@@ -0,0 +1,25 @@
+// 1. Import utilities from `astro:content`
+import { defineCollection, z } from 'astro:content';
+
+// 2. Import loader(s)
+import { glob } from 'astro/loaders';
+
+// "/src/pages/post/**/*.(md|mdx)"
+// "/src/thoughts/**/*.(json)"
+
+// 3. Define your collection(s)
+// const blog = defineCollection({
+//     loader: glob({ pattern: "**/*.(md|mdx)", base: "/src/pages/post" })
+// });
+const thoughts = defineCollection({
+    loader: glob({ pattern: "**/*.json", base: "./src/thoughts" }),
+    schema: z.object({
+        author: z.string(),
+        label: z.string(),
+        datetime: z.string().datetime(),
+        content: z.string(),
+    })
+});
+
+// 4. Export a single `collections` object to register your collection(s)
+export const collections = { thoughts };
